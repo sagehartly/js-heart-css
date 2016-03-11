863a5c52fdf59a7b6001d302ea8e93cd968d4579
sagehartly <sage.hartly@gmail.com>
2016-03-11 16:16:08 -0500

    Added jQuery

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $header = $('header');
+  $header.addClass('masthead');
+
+var $h1 = $('h1');
+  $h1.removeClass('big-heading');
+  $h1.addClass('logo');
+
+var $main = $('main');
+  $main.addClass('wrapper');
+var $p  = $('p')
