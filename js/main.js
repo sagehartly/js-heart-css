325d97948c20d347ee87aa6db9c4ed9d3d07d046
sagehartly <sage.hartly@gmail.com>
2016-03-11 17:01:44 -0500

    Finished

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 var $header = $('header');
-  $header.addClass('masthead');
-
 var $h1 = $('h1');
-  $h1.removeClass('big-heading');
-  $h1.addClass('logo');
-
 var $main = $('main');
-  $main.addClass('wrapper');
-var $p  = $('p')
+var $p  = $('p:first-child');
+var $h2 = $('h2');
+var $ul = $('ul');
+var $img = $('img');
+var $firstItem = $('li:first-child');
+var $lastItem = $('li:last-child');
+var $dl = $('dl');
+var $dt = $('dt');
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+$main.addClass('wrapper');
+$p.addClass('intro');
+$h2.addClass('slug-head');
+$ul.addClass('slug-list');
+$img.addClass('slug-img');
+$lastItem.addClass('slug-list-last');
+$dl.addClass('classification');
+$dt.removeClass('big-label');
+
+$lastItem.show();
